7728e4f2f321eb09b81b962b952e930bc87eb736
mrkrye <mrkrye@KRAMSEYER>
2024-03-20 15:28:33 -0700

    edits

--- server/index.js
+++ server/index.js
@@ -1,83 +1,83 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import request from "request";
+import express from "express";
+import dotenv from "dotenv";
+import request from "request";
 
-// const port = 5000;
+const port = 5000;
 
-// global.access_token = ""
+global.access_token = "";
 
-// dotenv.config();
+dotenv.config();
 
-// const client_id = process.env.CLIENT_ID;
-// const client_secret = process.env.CLIENT_SECRET;
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
 
-// var redirect_uri = "http://localhost:3000/auth/callback";
+var redirect_uri = "http://localhost:3000/auth/callback";
 
-// let access_token = "";
+let access_token = "";
 
-// const generateRandomString = (length) => {
-//   let text = "";
-//   const possible =
-//     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-//   for (let i = 0; i < length; i++) {
-//     text += possible.charAt(Math.floor(Math.random() * possible.length));
-//   }
-//   return text;
-// };
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
 
-// const app = express();
+const app = express();
 
-// app.get("/auth/login", (req, res) => {
-//   const scope = ["streaming", "user-read-email", "user-read-private"];
-//   const state = generateRandomString(16);
+app.get("/auth/login", (req, res) => {
+  const scope = ["streaming", "user-read-email", "user-read-private"];
+  const state = generateRandomString(16);
 
-//   const authQueryParameters = new URLSearchParams({
-//     response_type: "code",
-//     client_id: client_id,
-//     scope: scope,
-//     redirect_uri: redirect_uri,
-//     state: state,
-//   });
+  const authQueryParameters = new URLSearchParams({
+    response_type: "code",
+    client_id: client_id,
+    scope: scope,
+    redirect_uri: redirect_uri,
+    state: state,
+  });
 
-//   res.redirect(
-//     "https://accounts.spotify.com/authorize/?" + authQueryParameters.toString()
-//   );
-// });
+  res.redirect(
+    "https://accounts.spotify.com/authorize/?" + authQueryParameters.toString()
+  );
+});
 
-// app.get("/auth/callback", (req, res) => {
-//   const code = req.query.code;
+app.get("/auth/callback", (req, res) => {
+  const code = req.query.code;
 
-//   const authOptions = {
-//     url: "https://accounts.spotify.com/api/token",
-//     form: {
-//       code: code,
-//       redirect_uri: redirect_uri,
-//       grant_type: "authorization_code",
-//     },
-//     headers: {
-//       Authorization:
-//         "Basic " +
-//         Buffer.from(client_id + ":" + client_secret).toString("base64"),
-//       "Content-Type": "application/x-www-form-urlencoded",
-//     },
-//     json: true,
-//   };
+  const authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    form: {
+      code: code,
+      redirect_uri: redirect_uri,
+      grant_type: "authorization_code",
+    },
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(client_id + ":" + client_secret).toString("base64"),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    json: true,
+  };
 
-//   request.post(authOptions, function (error, response, body) {
-//     if (!error && response.statusCode === 200) {
-//       access_token = body.access_token;
-//       res.redirect("/");
-//     }
-//   });
-// });
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      access_token = body.access_token;
+      res.redirect("/");
+    }
+  });
+});
 
-// app.get("/auth/token", (req, res) => {
-//   res.json({ access_token: access_token });
-// });
+app.get("/auth/token", (req, res) => {
+  res.json({ access_token: access_token });
+});
 
-// app.listen(port, () => {
-//   console.log(`Listening at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
 
-// export default app;
+export default app;
